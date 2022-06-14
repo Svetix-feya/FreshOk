2264a53b0affcb975b41405cdca23bd4905df57a
svetix-feya <svetlana.sinitsyna2012@yandex.ru>
2022-06-15 00:19:20 +0300

    add catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 $(function(){
     $('.slider').slick({
-
+        prevArrow : '<button type="button" class="slick-prev"><img class="slick-prev__img "src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow : '<button type="button" class="slick-next"><img class="slick-next__img" src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
+        autoplay: true,
     });
 });
