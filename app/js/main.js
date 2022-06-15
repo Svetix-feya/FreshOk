9c2efb596f8b5e324e5d92f1b2e6c0caa231305b
svetix-feya <svetlana.sinitsyna2012@yandex.ru>
2022-06-15 23:49:15 +0300

    12 hw finish-1

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,10 @@
 $(function(){
-    $('.slider').slick({
+    $('.slider__items').slick({
         prevArrow : '<button type="button" class="slick-prev"><img class="slick-prev__img "src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow : '<button type="button" class="slick-next"><img class="slick-next__img" src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
-        autoplay: true,
+        autoplay: false,
     });
+
+    var mixer = mixitup('.topproducts');
+
 });
