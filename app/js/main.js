928df77b646df5504620e56538da20359229b7be
svetix-feya <svetlana.sinitsyna2012@yandex.ru>
2022-06-27 20:51:05 +0300

    fix 12 homework

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,9 @@
 $(function(){
-    $('.slider__items').slick({
+    $('.slider-top__items').slick({
         prevArrow : '<button type="button" class="slick-prev"><img class="slick-prev__img "src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow : '<button type="button" class="slick-next"><img class="slick-next__img" src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
-        autoplay: false,
+        autoplay: true,
+        infinite: false,
     });
 
     var mixer = mixitup('.topproducts');
