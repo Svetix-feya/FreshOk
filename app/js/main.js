a8b1c1ce9aa5733ca019968ad2494f2ef87be66d
svetix-feya <svetlana.sinitsyna2012@yandex.ru>
2022-11-06 20:22:16 +0300

    14 hw

--- app/js/main.js
+++ app/js/main.js
@@ -34,6 +34,40 @@ $(function(){
         ]
     });
 
+
+    var scrolled;
+    window.onscroll = function () {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 50) {
+            $(".header__bottom").addClass('header__bottom--active');
+            $(".usernav__cart").addClass('usernav__cart--active');
+        }
+        if (50 > scrolled) {
+            $(".header__bottom").removeClass('header__bottom--active');
+            $(".usernav__cart").removeClass('usernav__cart--active');
+        }
+    }
+
+
+    $('.usernav__item-cart, .usernav__close').on('click', function () {
+        $('.usernav__cart').toggleClass('hidden');  
+        $('.wrapper').toggleClass('wrapper--lock'); 
+    }); 
+
+    $('.usernav__clean-cart').on('click', function () {
+        $('.usernav__card').toggleClass('hidden');
+        
+    }); 
+
+
+    $('.product-card__close').on('click', function () {
+        $('.usernav__card').toggleClass('hidden');  
+    });
+
+
+    $('.product-card__button').styler();
+
+
     var containerEl1 = document.querySelector('[data-ref="top-products"]');
     var containerEl2 = document.querySelector('[data-ref="stocks"]');
  
@@ -52,32 +86,38 @@ $(function(){
     }); 
     
 
-    // $('.search-form').hasClass('search-form--active').on        ('click', function () {
-    //         $(this).toggleClass('search-form--active'); 
-    // });
+    $(document).on('mouseup', function (e) {
+        var search = $('.search-form--active')
+        if (!search.is(e.target) && search.has(e.target).length === 0) {
+            search.removeClass('search-form--active');
+        }
+    });
+
     
     $('.header__btn, .header__close').on('click', function () {
         $('.header__menu-nav').toggleClass('header__menu-nav--active');       
         $('.wrapper').toggleClass('wrapper--lock')
     });  
 
-    // document.addEventListener('DOMContentLoaded', () => {
-        
-    //     const burger = document.querySelector('.header__btn'); //наша кнопка
-    //     const mobileMenu = document.querySelector('.header__menu-nav'); //мобильное меню
-    //     const bodyLock = document.querySelector('.wrapper'); //ищем как селектор ТЕГА
-      
-    //     document.addEventListener('click', function (e) {
-    //         if (e.target !== burger && e.target !== mobileMenu) {                
-    //         mobileMenu.classList.remove('header__menu-nav--active');
-    //         bodyLock.classList.remove('.wrapper--lock');
-    //         }
-    //     });
-        
-    // });
+    $(document).on('mouseup', function (e) {
+        var search = $('.header__menu-nav--active')
+        if (!search.is(e.target) && search.has(e.target).length === 0) {
+            search.removeClass('header__menu-nav--active');
+        }
+    });
 
-    $('.usernav__item-cart, .usernav__close').on('click', function () {
-        $('.usernav__cart').toggleClass('hidden');  
-    }); 
+    $(document).on('mouseup', function (e) {
+        var wrapper = $('.wrapper--lock')
+        if (!wrapper.is(e.target) && wrapper.has(e.target).length === 0) {
+            wrapper.removeClass('wrapper--lock');
+        }
+    });
+
+    $(document).on('mouseup', function (e) {
+        var cart = $('.usernav__cart')
+        if (!cart.is(e.target) && cart.has(e.target).length === 0) {
+            cart.addClass('hidden');
+        }
+    });
 
 });
